555188d53aed0df0ca992292594f393289d189ff
Jesper Grönroos <jesper.gronroos@hotmail.com>
2023-10-02 10:16:45 +0300

    added ui

---
+++ frontend/src/LoadingIcon.js
@@ -0,0 +1,14 @@
+// LoadingIcon.js
+
+import React from 'react';
+import './LoadingIcon.css'; // Adjust the CSS file path as needed
+
+const LoadingIcon = () => {
+  return (
+    <div className="loading-icon">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default LoadingIcon;
---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HomePage from './HomePage';
+
+ReactDOM.render(
+  <HomePage />,
+  document.getElementById('root')
+);
